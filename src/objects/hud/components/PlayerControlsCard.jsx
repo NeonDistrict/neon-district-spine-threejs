0ed93f15bdc5ec9b25511cb9224c98797886c511
Cybourgeoisie <theocarina@gmail.com>
2021-02-25 13:23:45 -0500

    First pass on player controls

--- src/objects/hud/components/PlayerControlsCard.jsx
+++ src/objects/hud/components/PlayerControlsCard.jsx
@@ -3,16 +3,25 @@ import lstyle from "../../../styles/hud.scss";
 
 export class PlayerControlsCard extends Component {
 
+  isValidCard(card) {
+    return card && card.type && ['attack','ability','effect','interact'].indexOf(card.type.toLowerCase()) >= 0;
+  }
+
   render() {
     console.log("** Rendering the Player Controls Card **");
 
     let styles = [lstyle.card];
     let wrapperStyle = [lstyle.cardSelectionWrapper, this.props.selected ? lstyle.selected : ''].join(' ');
 
-    if (!this.props.card || !this.props.card.hasOwnProperty('cardId')) {
+    let card = this.props.card;
+
+    if (!this.isValidCard(card)) {
+      // Get the specific card type for styles
+      styles.push(lstyle.fillerCard);
+
       return (
         <div className={lstyle.cardSelectionWrapper}>
-          <div className={styles}>
+          <div className={styles.join(' ')}>
             <h3>Empty Card Slot</h3>
             <p>Missing Weapon Equipment</p>
           </div>
@@ -20,14 +29,25 @@ export class PlayerControlsCard extends Component {
       );
     }
 
-    let card = this.props.card;
+    // Get the specific card type for styles
     styles.push(lstyle[card.type.toLowerCase() + 'Card']);
 
     return (
       <div className={wrapperStyle} onClick={this.props.callback}>
         <div className={styles.join(' ')}>
-          <span className={[lstyle.cardType, lstyle[card.type.toLowerCase() + 'CardType']].join(' ')}></span>
-          <h3 className={lstyle.cardTitle}>{card.name}</h3>
+          <div className={lstyle.cardTopRow}>
+            <div className={lstyle.cardTypeWrapper}>
+              <span className={[lstyle.cardType, lstyle[card.type.toLowerCase() + 'CardType']].join(' ')}></span>
+            </div>
+            <h3 className={lstyle.cardTitle}>{card.name}</h3>
+            <span className={lstyle.cardTicks}>
+              <span className={lstyle.cardTicksNumber}>
+                {card.tickCost}
+              </span>
+              <hr className={[lstyle.cardTicksLine, lstyle[card.type.toLowerCase() + 'Card']].join(' ')} />
+              <span className={lstyle.cardTicksLabel}>Ticks</span>
+            </span>
+          </div>
           <p className={lstyle.cardEffects}>{card.effects}</p>
           <p className={lstyle.cardExploits}>{card.exploits}</p>
         </div>
--- src/objects/hud/components/PlayerControlsDefaultOptions.jsx
+++ src/objects/hud/components/PlayerControlsDefaultOptions.jsx
@@ -13,7 +13,18 @@ export class PlayerControlsDefaultOptions extends Component {
       <div className={lstyle.defaultSelections}>
         <div className={wrapperStyle}>
           <div className={styles} onClick={this.props.callback}>
-            <h3>Base Attack</h3>
+            {/* Ticks */}
+            <span className={lstyle.cardTicks}>
+              <span className={lstyle.cardTicksNumber}>40</span>
+              <hr className={[lstyle.cardTicksLine, lstyle.attackCard].join(' ')} />
+              <span className={lstyle.cardTicksLabel}>Ticks</span>
+            </span>
+
+            {/* Central Image */}
+            <img className={lstyle.baseAttackImg} src="https://neon-district-season-one.s3.amazonaws.com/assets/V5/64w/1x/Attack.png" />
+
+            {/* Header */}
+            <h3 className={lstyle.baseAttackTitle}>Base Attack</h3>
           </div>
         </div>
       </div>
