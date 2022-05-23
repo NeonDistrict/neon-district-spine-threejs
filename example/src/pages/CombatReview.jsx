a2a0ae7d8d9d845977cabd2496ec2b74e97b1dbb
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-05-23 18:41:27 -0300

    chore: enable combat on combat review

--- example/src/pages/CombatReview.jsx
+++ example/src/pages/CombatReview.jsx
@@ -1,18 +1,18 @@
-import React, { Component } from "react";
 import { NDCombatPlayer } from "neon-district-spine-threejs";
+import React, { Component } from "react";
 
 export default class CombatReview extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      'battleId' : 'test',
-      'playback' : true
+      battleId: "test",
+      playback: true
     };
   }
 
   componentDidMount() {
-    window.addEventListener("getBattleInformation", (e) => {
-      document.getElementById('battleId').value = e.detail.battleId;
+    window.addEventListener("getBattleInformation", e => {
+      document.getElementById("battleId").value = e.detail.battleId;
     });
   }
 
@@ -21,89 +21,101 @@ export default class CombatReview extends Component {
       <span>
         <label htmlFor="battleId">
           Battle ID:
-          <input type="text" style={{width: "20em", margin: "0 5px 0 2px"}} name="battleId" id="battleId" />
+          <input
+            type="text"
+            style={{ width: "20em", margin: "0 5px 0 2px" }}
+            name="battleId"
+            id="battleId"
+          />
         </label>
-        <button onClick={this.setBattleId.bind(this)}>
-          Update
-        </button>
+        <button onClick={this.setBattleId.bind(this)}>Update</button>
       </span>
     );
   }
 
   setBattleId() {
-    let el = document.getElementById('battleId');
-    this.setState({'battleId' : el.value, 'playback' : true});
+    let el = document.getElementById("battleId");
+    this.setState({ battleId: el.value, playback: true });
   }
 
   render() {
-    let characters = [{
-      'position'  : 0,
-      'nftId'     : "test0",
-      'scale'     : 'character',
-      'skin'      : 'Male',
-      'weapon'    : 'blkoriginpulsarv16-legendary',
-      'jsonFile'  : 'spine-output/character/MediumMaleHeavySkinTest-1-spine.json',
-      'outfit'   : ['male', 'uncontrolledpresaledemon', 'rare']
-    },{
-      'position' : 1,
-      'nftId'    : "test1",
-      'scale'    : 'character',
-      'skin'     : 'Male',
-      'weapon'   : 'blkglassshard-legendary',
-      'jsonFile'  : 'spine-output/character/MediumMaleHeavySkinTest-1-spine.json',
-      'outfit'   : ['male', 'uncontrolledpresaledoctor', 'legendary']
-    },{
-      'position'  : 2,
-      'nftId'     : "test2",
-      'scale'     : 'character',
-      'skin'      : 'Female',
-      'weapon'    : 'blkoriginpulsarv16-rare',
-      'jsonFile'  : 'spine-output/character/MediumMaleHeavySkinTest-1-spine.json',
-      'outfit'   : ['female', 'uncontrolledpresalejack', 'uncommon']
-    },{
-      'position' : 3,
-      'nftId'    : "test3",
-      'scale'    : 'character',
-      'skin'     : 'Female',
-      'weapon'   : 'blkpartnerrevelatorz17-ultrarare',
-      'jsonFile'  : 'spine-output/character/MediumMaleHeavySkinTest-1-spine.json',
-      'outfit'   : ['female', 'blkspecialneonpizzaagentr', 'legendary']
-    },{
-      'position' : 4,
-      'nftId'    : "test4",
-      'scale'    : 'character',
-      'skin'     : 'Female',
-      'weapon'   : 'blkspecialdeathknightsword-legendary',
-      'jsonFile'  : 'spine-output/character/MediumMaleHeavySkinTest-1-spine.json',
-      'outfit'   : ['female', 'blkspecialdeathknight', 'legendary']
-    },{
-      'position' : 5,
-      'nftId'    : "test5",
-      'scale'    : 'character',
-      'skin'     : 'Male',
-      'weapon'   : 'blkoriginsaltas-ultrarare',
-      'jsonFile'  : 'spine-output/character/MediumMaleHeavySkinTest-1-spine.json',
-      'outfit'   : ['male', 'uncontrolledpresaleghost', 'legendary', '000']
-    },{
-      'position' : 6,
-      'nftId'    : "test6",
-      'scale'    : 'character',
-      'skin'     : 'Male',
-      'weapon'   : 'blkprotectionorb-rare',
-      'jsonFile'  : 'spine-output/character/MediumMaleHeavySkinTest-1-spine.json',
-      'outfit'   : ['male', 'blkspecialneonpizzaagentp', 'legendary']
-    },{
-      'position' : 7,
-      'nftId'    : "test7",
-      'scale'    : 'character',
-      'skin'     : 'Male',
-      'weapon'   : 'blkbluebolt-uncommon',
-      'jsonFile'  : 'spine-output/character/MediumMaleHeavySkinTest-1-spine.json',
-      'outfit'   : ['male', 'plasmabearssuit', 'legendary']
-    }];
+    let characters = [
+      {
+        position: 0,
+        nftId: "test0",
+        scale: "character",
+        skin: "Male",
+        weapon: "blkoriginpulsarv16-legendary",
+        jsonFile: "spine-output/character/MediumMaleHeavySkinTest-1-spine.json",
+        outfit: ["male", "uncontrolledpresaledemon", "rare"]
+      },
+      {
+        position: 1,
+        nftId: "test1",
+        scale: "character",
+        skin: "Male",
+        weapon: "blkglassshard-legendary",
+        jsonFile: "spine-output/character/MediumMaleHeavySkinTest-1-spine.json",
+        outfit: ["male", "uncontrolledpresaledoctor", "legendary"]
+      },
+      // {
+      //   'position'  : 2,
+      //   'nftId'     : "test2",
+      //   'scale'     : 'character',
+      //   'skin'      : 'Female',
+      //   'weapon'    : 'blkoriginpulsarv16-rare',
+      //   'jsonFile'  : 'spine-output/character/MediumMaleHeavySkinTest-1-spine.json',
+      //   'outfit'   : ['female', 'uncontrolledpresalejack', 'uncommon']
+      // },{
+      //   'position' : 3,
+      //   'nftId'    : "test3",
+      //   'scale'    : 'character',
+      //   'skin'     : 'Female',
+      //   'weapon'   : 'blkpartnerrevelatorz17-ultrarare',
+      //   'jsonFile'  : 'spine-output/character/MediumMaleHeavySkinTest-1-spine.json',
+      //   'outfit'   : ['female', 'blkspecialneonpizzaagentr', 'legendary']
+      // },
+      {
+        position: 4,
+        nftId: "test4",
+        scale: "character",
+        skin: "Female",
+        weapon: "blkspecialdeathknightsword-legendary",
+        jsonFile: "spine-output/character/MediumMaleHeavySkinTest-1-spine.json",
+        outfit: ["female", "blkspecialdeathknight", "legendary"]
+      },
+      {
+        position: 5,
+        nftId: "test5",
+        scale: "character",
+        skin: "Male",
+        weapon: "blkoriginsaltas-ultrarare",
+        jsonFile: "spine-output/character/MediumMaleHeavySkinTest-1-spine.json",
+        outfit: ["male", "uncontrolledpresaleghost", "legendary", "000"]
+      }
+      // {
+      // 'position' : 6,
+      // 'nftId'    : "test6",
+      // 'scale'    : 'character',
+      // 'skin'     : 'Male',
+      // 'weapon'   : 'blkprotectionorb-rare',
+      // 'jsonFile'  : 'spine-output/character/MediumMaleHeavySkinTest-1-spine.json',
+      // 'outfit'   : ['male', 'blkspecialneonpizzaagentp', 'legendary']
+      // },
+      // {
+      // 'position' : 7,
+      // 'nftId'    : "test7",
+      // 'scale'    : 'character',
+      // 'skin'     : 'Male',
+      // 'weapon'   : 'blkbluebolt-uncommon',
+      // 'jsonFile'  : 'spine-output/character/MediumMaleHeavySkinTest-1-spine.json',
+      // 'outfit'   : ['male', 'plasmabearssuit', 'legendary']
+      // }
+    ];
 
     function getScreenSize() {
-      let width = 1536, height = 864;
+      let width = 1536,
+        height = 864;
 
       if (window.innerWidth < 1605) {
         width = 1440;
@@ -135,7 +147,7 @@ export default class CombatReview extends Component {
         height = 540;
       }
 
-      return {width, height}
+      return { width, height };
     }
 
     let size = getScreenSize();
