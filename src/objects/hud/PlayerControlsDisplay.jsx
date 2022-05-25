87f61feb4fc087a237634807046612db0d759594
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-05-25 16:10:25 -0300

    feat: render win/lose component when battle finish

--- src/objects/hud/PlayerControlsDisplay.jsx
+++ src/objects/hud/PlayerControlsDisplay.jsx
@@ -1,43 +1,62 @@
 import React from "react";
 import lstyle from "../../styles/hud.scss";
-
-import { HUDComponent } from './core/HUDComponent.jsx';
-import { PlayerControlsCharacter } from './components/PlayerControlsCharacter.jsx';
-import { PlayerControlsCard } from './components/card/PlayerControlsCard.jsx';
-import { TargetCharacterControls } from './components/TargetCharacterControls.jsx';
+import { BattleComplete } from "./components/battle-complete/index.jsx";
+import { PlayerControlsCard } from "./components/card/PlayerControlsCard.jsx";
+import { PlayerControlsCharacter } from "./components/PlayerControlsCharacter.jsx";
+import { TargetCharacterControls } from "./components/TargetCharacterControls.jsx";
+import { HUDComponent } from "./core/HUDComponent.jsx";
 
 export class PlayerControlsDisplay extends HUDComponent {
-
   constructor(props) {
     super(props);
 
     this.state = {
-      selectedAction : null,
-      selectedTarget : null,
-      activeCharacter : null
+      selectedAction: null,
+      selectedTarget: null,
+      activeCharacter: null,
+      battleComplete: false,
+      winner: null,
+      owner: null
     };
 
     this.confirmAction = props.confirmAction;
+
+    window.addEventListener("battleComplete", ({ detail }) => {
+      this.setState({
+        battleComplete: true,
+        winner: detail.winner,
+        owner: detail.owner
+      });
+    });
   }
 
   chooseOption(option) {
     if (this.state.selectedAction !== option) {
-      if (this.props.playerSelections && this.props.playerSelections.validateActionSelect(option)) {
+      if (
+        this.props.playerSelections &&
+        this.props.playerSelections.validateActionSelect(option)
+      ) {
         this.props.playerSelections.setAction(option);
       }
-      this.setState({selectedAction:option});
+      this.setState({ selectedAction: option });
     }
   }
 
   chooseReplaceOption(target) {
     // Remove visuals
-    this.setState({selectedAction:null, selectedAction:null});
+    this.setState({ selectedAction: null, selectedAction: null });
 
     // Select replace and the correct card target
-    if (this.props.playerSelections && this.props.playerSelections.validateActionSelect('replace')) {
-      this.props.playerSelections.setAction('replace');
+    if (
+      this.props.playerSelections &&
+      this.props.playerSelections.validateActionSelect("replace")
+    ) {
+      this.props.playerSelections.setAction("replace");
 
-      if (this.props.playerSelections && this.props.playerSelections.validateTargetSelect(target)) {
+      if (
+        this.props.playerSelections &&
+        this.props.playerSelections.validateTargetSelect(target)
+      ) {
         this.props.playerSelections.setTarget(target);
 
         // Submit the action
@@ -47,14 +66,17 @@ export class PlayerControlsDisplay extends HUDComponent {
   }
 
   chooseTarget(option) {
-    if (this.props.playerSelections && this.props.playerSelections.validateTargetSelect(option)) {
+    if (
+      this.props.playerSelections &&
+      this.props.playerSelections.validateTargetSelect(option)
+    ) {
       this.props.playerSelections.setTarget(option);
-      this.setState({selectedTarget: option});
+      this.setState({ selectedTarget: option });
     }
   }
 
   confirmActionCapture() {
-    this.setState({selectedAction:null, selectedAction:null});
+    this.setState({ selectedAction: null, selectedAction: null });
     this.confirmAction();
   }
 
@@ -66,16 +88,15 @@ export class PlayerControlsDisplay extends HUDComponent {
 
     return (
       <div className={lstyle.playerControlsWrapper}>
-        <div className={[lstyle.playerRegion, lstyle.bottomBorder].join(' ')}>
-
+        {this.state.battleComplete && (
+          <BattleComplete winner={this.state.winner} owner={this.state.owner} />
+        )}
+        <div className={[lstyle.playerRegion, lstyle.bottomBorder].join(" ")}>
           {/* Top Border */}
-          <div className={lstyle.topBorder}>
-            Player
-          </div>
+          <div className={lstyle.topBorder}>Player</div>
 
           {/* Middle Panel */}
           <div className={lstyle.playerControls}>
-
             {/* Player Profile */}
             <PlayerControlsCharacter
               character={activeCharacter}
@@ -97,36 +118,43 @@ export class PlayerControlsDisplay extends HUDComponent {
             {/* Cards */}
             <div className={lstyle.cardWrapper}>
               <PlayerControlsCard
-                card={this.props.playerSelections && this.props.playerSelections.getCard(0) || {}}
-                callback={this.chooseOption.bind(this, 'card0')}
-                replaceCallback={this.chooseReplaceOption.bind(this, 'card0')}
-                selected={this.state.selectedAction === 'card0'}
+                card={
+                  (this.props.playerSelections &&
+                    this.props.playerSelections.getCard(0)) ||
+                  {}
+                }
+                callback={this.chooseOption.bind(this, "card0")}
+                replaceCallback={this.chooseReplaceOption.bind(this, "card0")}
+                selected={this.state.selectedAction === "card0"}
               />
 
               <PlayerControlsCard
-                card={this.props.playerSelections && this.props.playerSelections.getCard(1) || {}}
-                callback={this.chooseOption.bind(this, 'card1')}
-                replaceCallback={this.chooseReplaceOption.bind(this, 'card1')}
-                selected={this.state.selectedAction === 'card1'}
+                card={
+                  (this.props.playerSelections &&
+                    this.props.playerSelections.getCard(1)) ||
+                  {}
+                }
+                callback={this.chooseOption.bind(this, "card1")}
+                replaceCallback={this.chooseReplaceOption.bind(this, "card1")}
+                selected={this.state.selectedAction === "card1"}
               />
 
               <PlayerControlsCard
-                card={this.props.playerSelections && this.props.playerSelections.getCard(2) || {}}
-                callback={this.chooseOption.bind(this, 'card2')}
-                replaceCallback={this.chooseReplaceOption.bind(this, 'card2')}
-                selected={this.state.selectedAction === 'card2'}
+                card={
+                  (this.props.playerSelections &&
+                    this.props.playerSelections.getCard(2)) ||
+                  {}
+                }
+                callback={this.chooseOption.bind(this, "card2")}
+                replaceCallback={this.chooseReplaceOption.bind(this, "card2")}
+                selected={this.state.selectedAction === "card2"}
               />
             </div>
-
           </div>
-
         </div>
-        <div className={[lstyle.targetRegion, lstyle.bottomBorder].join(' ')}>
-
+        <div className={[lstyle.targetRegion, lstyle.bottomBorder].join(" ")}>
           {/* Top Border */}
-          <div className={lstyle.topBorder}>
-            Target
-          </div>
+          <div className={lstyle.topBorder}>Target</div>
 
           {/* Middle Panel */}
           <div className={lstyle.targetControls}>
@@ -147,5 +175,4 @@ export class PlayerControlsDisplay extends HUDComponent {
       </div>
     );
   }
-
 }
